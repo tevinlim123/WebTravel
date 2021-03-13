adb238e639a91cc434030f1e53714f108601d513
tevinlim123 <tevinlim55@gmail.com>
2021-03-13 19:10:24 +0700

    Initial commit

---
+++ WebTravel/Controllers/WebTravelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTravel.Controllers
+{
+    public class WebTravelController : Controller
+    {
+        // GET: WebTravel
+        public ActionResult Data()
+        {
+            return View();
+        }
+        public ActionResult Data2()
+        {
+            return View();
+        }
+    }
+}
